5cc5a68eb24f6d9da428ab2f265fdfb7897d41d8
stefany.cardenas <stefany.cardenas@pragma.com.co>
2024-01-08 20:51:30 -0500

    build: initialize project with Next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.scss";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dictionary web app",
+  description: "Frontend Mentor | Dictionary web app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
